51b5508e56e8d2bb390da8abf0cdb63d1f829f29
sdchriste <126285294+sdchriste@users.noreply.github.com>
2023-11-01 15:27:03 -0400

    Branch 1

--- main.tf
+++ main.tf
@@ -1,1 +1,20 @@
 # Main.tf v0 - Brnach Checkpoint1 
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.97.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "sec-rg" {
+  name     = "rg-tfc"
+  location = "East Us"
+ 
+}
