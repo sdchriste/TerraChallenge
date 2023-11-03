e51249eab21f007227d394b6d2f62a0a9ce698bb
sdchriste <126285294+sdchriste@users.noreply.github.com>
2023-11-03 11:27:22 -0400

    main3

--- main.tf
+++ main.tf
@@ -29,21 +29,21 @@ resource "azurerm_virtual_network" "sec-vn" {
   }
 
   resource "azurerm_subnet" "sec-subnet1" {
-  name                 = "tfc1-subnet"
+  name                 = "Web"
   resource_group_name  = azurerm_resource_group.sec-rg.name
   virtual_network_name = azurerm_virtual_network.sec-vn.name
   address_prefixes     = ["10.123.1.0/24"]
   }
 
   resource "azurerm_subnet" "sec-subnet2" {
-  name                 = "tfc2-subnet"
+  name                 = "Data"
   resource_group_name  = azurerm_resource_group.sec-rg.name
   virtual_network_name = azurerm_virtual_network.sec-vn.name
   address_prefixes     = ["10.123.2.0/24"]
   }
 
   resource "azurerm_subnet" "sec-subnet3" {
-  name                 = "tfc3-subnet"
+  name                 = "JumpBox"
   resource_group_name  = azurerm_resource_group.sec-rg.name
   virtual_network_name = azurerm_virtual_network.sec-vn.name
   address_prefixes     = ["10.123.3.0/24"]
