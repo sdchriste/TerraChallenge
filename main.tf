8766ebfd4d50b80c9ae8526ebc00f7e5d6eec106
sdchriste <126285294+sdchriste@users.noreply.github.com>
2023-11-03 11:14:34 -0400

    main2

--- main.tf
+++ main.tf
@@ -19,3 +19,32 @@ resource "azurerm_resource_group" "sec-rg" {
   location = "East Us"
  
 }
+
+resource "azurerm_virtual_network" "sec-vn" {
+  name                = "tfc-network"
+  resource_group_name = azurerm_resource_group.rg-tfc.name
+  location            = azurerm_resource_group.rg-tfc.location
+  address_space       = ["10.123.0.0/16"]
+
+  }
+
+  resource "azurerm_subnet" "sec-subnet1" {
+  name                 = "tfc1-subnet"
+  resource_group_name  = azurerm_resource_group.tfc-rg.name
+  virtual_network_name = azurerm_virtual_network.sec-vn.name
+  address_prefixes     = ["10.123.1.0/24"]
+  }
+
+  resource "azurerm_subnet" "sec-subnet2" {
+  name                 = "tfc2-subnet"
+  resource_group_name  = azurerm_resource_group.tfc-rg.name
+  virtual_network_name = azurerm_virtual_network.sec-vn.name
+  address_prefixes     = ["10.123.2.0/24"]
+  }
+
+  resource "azurerm_subnet" "sec-subnet3" {
+  name                 = "tfc3-subnet"
+  resource_group_name  = azurerm_resource_group.tfc-rg.name
+  virtual_network_name = azurerm_virtual_network.sec-vn.name
+  address_prefixes     = ["10.123.3.0/24"]
+  }
