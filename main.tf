3703afcdec9c5f4fc7b7b04346482d3d962d78e1
sdchriste <126285294+sdchriste@users.noreply.github.com>
2023-11-06 11:59:19 -0500

    monday1

--- main.tf
+++ main.tf
@@ -48,3 +48,17 @@ resource "azurerm_virtual_network" "sec-vn" {
   virtual_network_name = azurerm_virtual_network.sec-vn.name
   address_prefixes     = ["10.123.3.0/24"]
   }
+
+  resource "azurerm_network_interface" "sec-nic" {
+  name                = "lnx1-nic"
+  location            = azurerm_resource_group.sec-rg.location
+  resource_group_name = azurerm_resource_group.sec-rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.web.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.lnx1-pip.id
+    
+  }
+}
